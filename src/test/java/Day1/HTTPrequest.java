64fa9466d9b482f3d472ccc850f43ede2b39ea67
SpyFox <altunin.vitaly@example.com>
2023-08-18 15:03:40 +0400

    Обучение день-1_3

--- src/test/java/Day1/HTTPrequest.java
+++ src/test/java/Day1/HTTPrequest.java
@@ -2,13 +2,13 @@ package Day1;
 
 /*
 given()
-    content type, set cookies
+    content type, set cookies, add auth, add param, set headers info
 
 when()
     get, post, put, delete
 
 then()
-    validate status code
+    validate status code, extract response, extract headers cookies & response body
 */
 
 import org.testng.annotations.Test;
@@ -28,33 +28,31 @@ public class HTTPrequest {
     {
         given()
                 .when()
-                .get("https://reqres.in/api/users?page=2")
+                    .get("https://reqres.in/api/users?page=2")
 
                 .then()
-                .statusCode(200)
-                .body("page",equalTo(2))
-                .log().all();
-
+                    .statusCode(200)
+                    .body("page",equalTo(2))
+                    .log().all();
 
     }
 
-    @Test (priority=2)
-    void createUser()
-    {
-        HashMap data = new HashMap<>();
-        data.put("name","pavan");
-        data.put("job","trainer");
+    @Test(priority = 2)
+    void createUser() {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("name", "pavan");
+        data.put("job", "trainer");
 
+        id = given()
+                    .contentType("application/json")
+                    .body(data)
+                .when()
+                    .post("https://reqres.in/api/users")
+                .then()
+                    .statusCode(201)
+                    .extract().jsonPath().getInt("id");
 
-        id=given()
-                .contentType("application/json")
-                .body(data)
-
-            .when()
-                .post("https://reqres.in/api/users")
-                .jsonPath().getInt("id");
-
-
+        System.out.println("Created user with ID: " + id);
     }
 
     @Test (priority=3,dependsOnMethods = {"createUser"})
@@ -68,12 +66,44 @@ public class HTTPrequest {
                 .contentType("application/json")
                 .body(data)
 
-            .when()
-                .put("https://reqres.in/api/users/"+id)
+                .when()
+                    .put("https://reqres.in/api/users/"+id)
 
-            .then()
-                .statusCode(200)
+                .then()
+                    .statusCode(200)
+                    .log().all();
+
+        System.out.println("Update user with ID: " + id);
+
+    }
+
+    @Test (priority=4,dependsOnMethods = {"createUser"})
+    void deleteUser()
+    {
+        given()
+
+                .when()
+                    .delete("https://reqres.in/api/users/"+id)
+
+                .then()
+                    .statusCode(204)
+                    .log().all();
+
+
+    }
+
+    @Test (priority=5,dependsOnMethods = {"deleteUser"})
+    void UserNotFound()
+    {
+        given()
+
+                .when()
+                .get("https://reqres.in/api/users/"+id)
+
+                .then()
+                .statusCode(404)
                 .log().all();
+
 
     }
 
